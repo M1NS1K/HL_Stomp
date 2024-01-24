e00f26058154b414408255ffa09b53f795f969f6
M1NS1K <rlaalstlr2001@gmail.com>
2024-01-24 18:41:18 +0900

    chat-message-dto

---
+++ src/main/java/com/hacker/hl/project/db/ChatMessageDTO.java
@@ -0,0 +1,13 @@
+package com.hacker.hl.project.db;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessageDTO {
+
+    private String roomId;
+    private String writer;
+    private String message;
+}
