cd75c0edd6ff3388541c8998e772a2334293917a
M1NS1K <rlaalstlr2001@gmail.com>
2024-01-24 18:40:07 +0900

    web-socket-config-ano-1

--- src/main/java/com/hacker/hl/project/config/StompWebSocketConfig.java
+++ src/main/java/com/hacker/hl/project/config/StompWebSocketConfig.java
@@ -8,19 +8,20 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-
-        registry.addEndpoint("/example").withSockJS();
+        registry.addEndpoint("/stomp/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
     }
 
+    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-
-        config.setApplicationDestinationPrefixes("/test");
-        config.enableSimpleBroker("/topic", "/queue");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
     }
 }
 
