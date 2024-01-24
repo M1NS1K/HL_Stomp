8ef61995c7df10128ab3b89bde94f9b95e6eef7f
M1NS1K <rlaalstlr2001@gmail.com>
2024-01-24 18:53:04 +0900

    stomp-chat-controller

--- src/main/java/com/hacker/hl/project/controller/StompChatController.java
+++ src/main/java/com/hacker/hl/project/controller/StompChatController.java
@@ -1,9 +1,25 @@
 package com.hacker.hl.project.controller;
 
+import com.hacker.hl.project.db.ChatMessageDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
 public class StompChatController {
+
+    private final SimpMessagingTemplate template;
+
+    @MessageMapping(value = "/chat/enter")
+    public void enter(ChatMessageDTO message) {
+        message.setMessage(message.getWriter() + "님이 채팅방에 참여하였습니다.");
+        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
+
+    @MessageMapping(value = "/chat/message")
+    public void message(ChatMessageDTO message){
+        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
 }
