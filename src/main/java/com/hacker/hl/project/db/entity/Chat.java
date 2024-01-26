481455fa39ba3da342ff08619d27fe35f6bc2266
M1NS1K <rlaalstlr2001@gmail.com>
2024-01-26 16:04:02 +0900

    entity : chat 구현

--- src/main/java/com/hacker/hl/project/db/entity/Chat.java
+++ src/main/java/com/hacker/hl/project/db/entity/Chat.java
@@ -1,12 +1,45 @@
 package com.hacker.hl.project.db.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Builder
+@AllArgsConstructor
 public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "room_id")
+    private ChatRoom room;
+
+    private String sender;
+
+    private String senderEmail;
+
+    @Column(columnDefinition = "TEXT")
+    private String message;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime sendDate;
 }
