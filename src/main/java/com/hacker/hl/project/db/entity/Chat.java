943e5fd800d898a65ecc39e917ea3e95b8148eec
M1NS1K <rlaalstlr2001@gmail.com>
2024-01-26 15:56:27 +0900

    build.gradle setting

---
+++ src/main/java/com/hacker/hl/project/db/entity/Chat.java
@@ -0,0 +1,12 @@
+package com.hacker.hl.project.db.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Chat {
+}
